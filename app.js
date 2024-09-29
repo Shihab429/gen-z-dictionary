84da8dcd18236dfa36e554cc3329fdb06579575d
Shihab429 <shikdarshihabali@gmail.com>
2024-09-29 16:48:48 +0600

    new word added and added empty box massage

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ fetch('allWord.json')
 
 var alldata;
 let show = document.getElementById('show');
+let inputtedWord = document.getElementById('wordInput');
 
 let searchWord = () => {
+        if(!inputtedWord.value){
+            show.innerHTML = `<h2 style="color: red; opacity: 0.9; margin-top: 20px; text-align: center;">Enter a word</h2>`
+        }
+        else{
+            let inputtedWordLow = inputtedWord.value.toLowerCase();
+            let foundWord = alldata.find( (eachWord) => {
+               return eachWord.word.toLowerCase() === inputtedWordLow;
+            })
         
-        let inputtedWord = document.getElementById('wordInput');
-        let inputtedWordLow = inputtedWord.value.toLowerCase();
-        let foundWord = alldata.find( (eachWord) => {
-           return eachWord.word.toLowerCase() === inputtedWordLow;
-        })
-    
-    show.innerHTML = `বাংলা অর্থ: <br> <h3> ${foundWord.ban_des} </h3> <br> English meaning: <br> <h3>${foundWord.eng_des} </h3> <br> উদাহরণ: <h3>${foundWord.ban_example} </h3> <br> Example: <h3> </h3>`
+        show.innerHTML = `বাংলা অর্থ: <br> <h3> ${foundWord.ban_des} </h3> <br> English meaning: <br> <h3>${foundWord.eng_des} </h3> <br> উদাহরণ: <h3>${foundWord.ban_example} </h3> <br> Example: <h3>${foundWord.eng_example} </h3>`
+        }
+     
     
 
 }
